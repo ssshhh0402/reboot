d3c45de225b1fcfbf6a77c1e23e46f22ccf5e6a5
ssshhh0402 <ssshhh0402@naver.com>
2019-09-09 15:35:41 +0900

    20190909

--- Articles/urls.py
+++ Articles/urls.py
@@ -3,4 +3,11 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
+    path('detail/<int:g_pk>', views.detail),
+    path('delete/<int:g_pk>', views.delete),
+    path('fix/<int:g_pk>', views.fix),
+    path('edit/<int:g_pk>', views.edit),
+    path('delete_all', views.delete_all),
 ]
--- Articles/views.py
+++ Articles/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Articles
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')urlpatterns = [
-   
-]
+    a = Articles.objects.all()
+    context = {
+        'a': a
+    }
+    return render(request, 'Articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'Articles/new.html')
+
+
+def create(request):
+    a = request.GET
+    article = Articles()
+    article.title = a.get('title')
+    article.content = a.get('content')
+    article.save()
+    return render(request, 'Articles/create.html')
+
+
+def detail(request, g_pk):
+    a = Articles.objects.get(pk=g_pk)
+    context = {
+        'a': a
+    }
+
+    return render(request, 'Articles/detail.html', context)
+
+
+def delete(request, g_pk):
+    a = Articles.objects.get(pk=g_pk)
+    a.delete()
+    return render(request, 'Articles/delete.html')
+
+
+def fix(request, g_pk):
+    a = Articles.objects.get(pk=g_pk)
+    context = {
+        'a': a
+    }
+    return render(request, 'Articles/fix.html', context)
+
+
+def edit(request, g_pk):
+    a = Articles.objects.get(pk=g_pk)
+    a.title = request.GET.get('title')
+    a.content = request.GET.get('content')
+    a.save()
+    return render(request, 'Articles/create.html')
+
+
+def delete_all(request):
+    Articles.objects.all().delete()
+    return redirect('/Articles')
